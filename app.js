0997b52e57390b21d40710b805f41ce99d55e5a4
Tyler Brinkman <tbrinkman3@gmail.com>
2021-01-03 15:08:51 -0600

    very rough working determine winner

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ $(document).ready(function() {
 
   var playGame = function() {
 
-    var count = 1;
+    var count = 0;
 
 
+    var checkWinner = function() {
+      if (count > 6) {
+        $square1 = $('#square1').text()
+        $square2 = $('#square2').text()
+        $square3 = $('#square3').text()
 
+        if ($square1 === $square2 && $square2 === $square3) {
+          alert($square1 + ' Wins!')
+        }
+      }
+
+    }
 
 
     $square.on('click', function() {
-      if (count >= 9) {
+      if (count >= 8) {
         alert('End of Game')
       }
       var id = $(this).attr('id');
+      //console.log(id)
       if(count % 2 === 0) {
         $('#'+id).text('X');
         count++;
@@ -23,6 +35,8 @@ $(document).ready(function() {
       $('#'+id).text('O');
       count++;
       }
+
+      checkWinner()
 
     })
 

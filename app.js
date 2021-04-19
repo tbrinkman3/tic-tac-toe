761cab144764e0c6ae8daee24b41a90fd3239d0c
Tyler Brinkman <tbrinkman3@gmail.com>
2021-04-18 19:47:43 -0500

    Added placer function with class names and count tracker

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hey')
+//Selectors
+var cells = document.querySelectorAll('table td');
+
+
+//Helper functions
+var count = 1;
+
+var isEven = number => number % 2 === 0 ? true : false;
+
+
+
+var placer = (e) => {
+  var target = e.target;
+  if (!target.classList.contains('clicked')) {
+    if (!isEven(count)) {
+      target.innerHTML = 'X';
+      target.classList.add('clicked');
+      count++;
+    } else {
+      target.innerHTML = 'O';
+      target.classList.add('clicked');
+      count++;
+    }
+  }
+}
+
+
+
+
+cells.forEach(cell => {
+  cell.addEventListener('click', e => placer(e))
+})
+
